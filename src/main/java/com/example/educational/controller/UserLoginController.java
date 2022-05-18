180f92e5fa1c80ebd252c0b530230b34fb03124e
LoyaltyK <717588228@qq.com>
2022-05-18 20:44:44 +0800

    添加基础信息

--- src/main/java/com/example/educational/controller/UserLoginController.java
+++ src/main/java/com/example/educational/controller/UserLoginController.java
@@ -1,4 +1,5 @@
 package com.example.educational.controller;
 
 public class UserLoginController {
+
 }
---
+++ src/main/java/com/example/educational/entity/Student.java
@@ -0,0 +1,13 @@
+package com.example.educational.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private int userId;
+
+}
