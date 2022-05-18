c27ab3841d9f5364389fe7be827875fcdf480fa9
LoyaltyK <717588228@qq.com>
2022-05-18 20:53:21 +0800

    添加基础信息

--- src/main/java/com/example/educational/EducationalApplication.java
+++ src/main/java/com/example/educational/EducationalApplication.java
@@ -1,9 +1,11 @@
 package com.example.educational;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.educational.mapper")
 public class EducationalApplication {
 
     public static void main(String[] args) {
